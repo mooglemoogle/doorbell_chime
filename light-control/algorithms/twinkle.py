9d550612a5e13fbaad883a7d8ff8130c41c7a108
Chris Rock <chris.rock@logianalytics.com>
2021-12-08 23:57:45 -0500

    Actually choose twinkles every so often

--- light-control/algorithms/twinkle.py
+++ light-control/algorithms/twinkle.py
@@ -9,7 +9,8 @@ class Algorithm(Algorithm):
         super().__init__(name, num_pixels, alg_config, settings)
         self.num_twinkles = math.ceil(settings['density'] * num_pixels)
         self.freq = settings['freq']
-        self.fade_per_cycle = 1.0 / (settings['fadeTime'] * self.refresh_rate())
+        self.since_choose = 0
+        self.fade_time = settings['fadeTime']
         self.twinkles = [0] * num_pixels
         
         self.choose_twinkles()
@@ -18,7 +19,7 @@ class Algorithm(Algorithm):
             pixel = self.pixels[n]
             pixel.hue = 0.0
             pixel.sat = 0.0
-            pixel.val = self.twinkles[n]
+            pixel.val = 0.0
     
     def choose_twinkles(self):
         new_twinkles = random.sample(range(len(self.pixels)), k=self.num_twinkles)
@@ -28,10 +29,17 @@ class Algorithm(Algorithm):
                 self.twinkles[n] = 1.0
     
     def run_cycle(self, _, elapsed_seconds):
+        self.since_choose += elapsed_seconds
+        if self.since_choose > self.freq:
+            self.since_choose -= self.freq
+            self.choose_twinkles()
+        
+        fade_amount = elapsed_seconds / self.fade_time
+
         for i in range(len(self.pixels)):
             pixel = self.pixels[i]
             if self.twinkles[i] > 0:
-                self.twinkles[i] = max(0, self.twinkles[i] - self.fade_per_cycle)
+                self.twinkles[i] = max(0, self.twinkles[i] - fade_amount)
             
             pixel.val = self.twinkles[i]
         
