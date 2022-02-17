a16c1c7ccb452c4b10630b6bef88497e19d519b4
Chris Rock <chris@abysmalsoft.org>
2022-02-17 15:46:34 -0500

    hopefully make server more resilient

--- web/server/dist/routes/actions.js
+++ web/server/dist/routes/actions.js
@@ -12,23 +12,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const zeromq_1 = require("zeromq");
 const sock = new zeromq_1.Request();
 sock.connect('tcp://localhost:5555');
+const isCommandResponse = (response) => {
+    return response.accepted !== undefined;
+};
+let sendLock = Promise.resolve('');
 const sendCommand = (command, options = {}) => __awaiter(void 0, void 0, void 0, function* () {
     const message = Object.assign({ command }, options);
-    yield sock.send(JSON.stringify(message));
-    const [result] = yield sock.receive();
-    try {
-        const resultObj = JSON.parse(result.toString('utf-8'));
-        return resultObj;
-    }
-    catch (_a) {
-        return result.toString('utf-8');
-    }
+    sendLock = sendLock.then(() => __awaiter(void 0, void 0, void 0, function* () {
+        yield sock.send(JSON.stringify(message));
+        const [result] = yield sock.receive();
+        try {
+            const resultObj = JSON.parse(result.toString('utf-8'));
+            return resultObj;
+        }
+        catch (_a) {
+            return result.toString('utf-8');
+        }
+    }));
+    return sendLock;
 });
 const VALID_COMMANDS = ['on', 'off', 'next'];
 exports.default = (router) => {
     router.get('/api/actions/get_status', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield sendCommand('get_status');
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(200);
             res.send(result);
         }
@@ -39,7 +46,7 @@ exports.default = (router) => {
     }));
     router.get('/api/actions/get_cycles', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield sendCommand('get_cycles');
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(200);
             res.send(result);
         }
@@ -50,7 +57,7 @@ exports.default = (router) => {
     }));
     router.post('/api/actions/set_brightness/:new_brightness', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield sendCommand('set_brightness', { brightness: req.params.new_brightness });
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(202);
             res.send(result);
         }
@@ -61,7 +68,7 @@ exports.default = (router) => {
     }));
     router.post('/api/actions/set_cycle/:new_cycle', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield sendCommand('set_cycle', { name: req.params.new_cycle });
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(202);
             res.send(result);
         }
@@ -74,7 +81,7 @@ exports.default = (router) => {
         const command = req.params.command.toLowerCase();
         if (VALID_COMMANDS.includes(command)) {
             const result = yield sendCommand(command);
-            if (result.accepted) {
+            if (isCommandResponse(result) && result.accepted) {
                 res.status(202);
                 res.send(result);
             }
--- web/server/src/routes/actions.ts
+++ web/server/src/routes/actions.ts
@@ -4,16 +4,29 @@ import { Request } from 'zeromq';
 const sock = new Request();
 sock.connect('tcp://localhost:5555');
 
+type CommandResponse = {
+    accepted: boolean;
+};
+
+const isCommandResponse = (response: string | CommandResponse): response is CommandResponse => {
+    return (response as CommandResponse).accepted !== undefined;
+};
+
+let sendLock: Promise<CommandResponse | string> = Promise.resolve('');
+
 const sendCommand = async (command: string, options = {}) => {
     const message = { command, ...options };
-    await sock.send(JSON.stringify(message));
-    const [result] = await sock.receive();
-    try {
-        const resultObj = JSON.parse(result.toString('utf-8'));
-        return resultObj;
-    } catch {
-        return result.toString('utf-8');
-    }
+    sendLock = sendLock.then(async () => {
+        await sock.send(JSON.stringify(message));
+        const [result] = await sock.receive();
+        try {
+            const resultObj = JSON.parse(result.toString('utf-8'));
+            return resultObj as CommandResponse;
+        } catch {
+            return result.toString('utf-8');
+        }
+    });
+    return sendLock;
 };
 
 const VALID_COMMANDS = ['on', 'off', 'next'];
@@ -22,7 +35,7 @@ export default (router: Router) => {
     router.get('/api/actions/get_status', async (req, res) => {
         const result = await sendCommand('get_status');
 
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(200);
             res.send(result);
         } else {
@@ -33,7 +46,7 @@ export default (router: Router) => {
     router.get('/api/actions/get_cycles', async (req, res) => {
         const result = await sendCommand('get_cycles');
 
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(200);
             res.send(result);
         } else {
@@ -44,7 +57,7 @@ export default (router: Router) => {
     router.post('/api/actions/set_brightness/:new_brightness', async (req, res) => {
         const result = await sendCommand('set_brightness', { brightness: req.params.new_brightness });
 
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(202);
             res.send(result);
         } else {
@@ -55,7 +68,7 @@ export default (router: Router) => {
     router.post('/api/actions/set_cycle/:new_cycle', async (req, res) => {
         const result = await sendCommand('set_cycle', { name: req.params.new_cycle });
 
-        if (result.accepted) {
+        if (isCommandResponse(result) && result.accepted) {
             res.status(202);
             res.send(result);
         } else {
@@ -68,7 +81,7 @@ export default (router: Router) => {
         if (VALID_COMMANDS.includes(command)) {
             const result = await sendCommand(command);
 
-            if (result.accepted) {
+            if (isCommandResponse(result) && result.accepted) {
                 res.status(202);
                 res.send(result);
             } else {
